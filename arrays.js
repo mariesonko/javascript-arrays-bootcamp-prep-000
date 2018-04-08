4994c21413692ed8feaebe02536ec8032cb703ca
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 01:06:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray (array, element)
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray (array, element)
-{ 
+{
 array.unshift(element)
   return array
 }
@@ -40,4 +40,3 @@ function removeElementFromEndOfArray (array)
 {
   return array.slice(0, array.length-1)
 }
-
